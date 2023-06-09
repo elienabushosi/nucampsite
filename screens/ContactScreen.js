80e5ed2616ffe87882ac533c3c6694849d4b0062
Elie <elienabushosi7@gmail.com>
2023-06-09 06:05:13 -0700

    2. Exercise: Set Up Redux complete

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,18 +1,18 @@
-// import { ScrollView } from "react-native-gesture-handler";
-// import { Card } from "react-native-elements";
-// import { Text } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
+import { Card } from "react-native-elements";
+import { Text } from "react-native-elements";
 
-// const ContactScreen = () => {
-//     return (
-//         <ScrollView>
-//             <Card wrapperStyle={{ margin: 20 }}>
-//                 <Card.Title>Contact Information</Card.Title>
-//                 <Card.Divider></Card.Divider>
-//                 <Text>Phone: 1-206-555-1234</Text>
-//                 <Text>Email: campsites@nucamp.co</Text>
-//             </Card>
-//         </ScrollView>
-//     );
-// };
+const ContactScreen = () => {
+    return (
+        <ScrollView>
+            <Card wrapperStyle={{ margin: 20 }}>
+                <Card.Title>Contact Information</Card.Title>
+                <Card.Divider></Card.Divider>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </Card>
+        </ScrollView>
+    );
+};
 
-// export default ContactScreen;
+export default ContactScreen;
